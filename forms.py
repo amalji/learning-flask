3f6c2e17a2966df00377e34a1ff8351114dff8c6
Amal Abdul Majeed <amalji@gmail.com>
2017-10-02 21:30:51 +0530

    Linking database to the signup page

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First Name', validators=[DataRequired("Please enter your First Name")])
+    last_name = StringField('Last Name', validators=[DataRequired("Please enter your Last Name")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email"), Email("Please enter your email in the correct format!")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password"), Length(min=8, message="Password should be atleast 8 characters long.")])
     submit = SubmitField('Sign Up')
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
@@ -18,10 +18,19 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
-    return render_template("signup.html", form=form)
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return "Success"
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',debug=True)
